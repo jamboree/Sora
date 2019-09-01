582d448e8c8f70a492be2a6cf24f47e1038a8a9c
Pierre van Houtryve <pierre.vanhoutryve@outlook.com>
2019-09-01 16:11:36 +0200

    Adding more Node declarations

--- include/Sora/AST/TypeRepr.hpp
+++ include/Sora/AST/TypeRepr.hpp
@@ -4,13 +4,21 @@
 //
 // Copyright (c) 2019 Pierre van Houtryve
 //===----------------------------------------------------------------------===//
-// TypeReprs are quite literal Type Representations. They represent types as
+// TypeReprs are, literally, Type Representations. They represent types as
 // they were written by the user. TypeReprs aren't types, but they are converted
 // to types during semantic analysis.
+//
+// This is used to have a representation of types that are source-accurate and
+// include source location information.
 //===----------------------------------------------------------------------===//
 
 #pragma once
 
-namespace sora {
+#include <stdint.h>
 
+namespace sora {
+enum class TypeReprKind : uint8_t {
+#define TYPEREPR(KIND, PARENT) KIND,
+#include "Sora/AST/TypeReprNodes.def"
+};
 }
