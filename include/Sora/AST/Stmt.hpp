fbfd84ee4151b4ce95b3fab4b673cabb23031d50
Pierre van Houtryve <pierre.vanhoutryve@outlook.com>
2019-09-06 11:45:56 +0200

    Adding base Stmt class

--- include/Sora/AST/Stmt.hpp
+++ include/Sora/AST/Stmt.hpp
@@ -7,11 +7,52 @@
 
 #pragma once
 
+#include "Sora/AST/ASTAlignement.hpp"
+#include "Sora/Common/SourceLoc.hpp"
+#include <cassert>
 #include <stdint.h>
 
 namespace sora {
+class Expr;
+class ASTContext;
+
+/// Kinds of Statements
 enum class StmtKind : uint8_t {
 #define STMT(KIND, PARENT) KIND,
 #include "Sora/AST/StmtNodes.def"
 };
-}
+
+/// Base class for every Statement node.
+class alignas(StmtAlignement) Stmt {
+  // Disable vanilla new/delete for statements
+  void *operator new(size_t) noexcept = delete;
+  void operator delete(void *)noexcept = delete;
+
+  StmtKind kind;
+
+protected:
+  // Children should be able to use placement new, as it is needed for children
+  // with trailing objects.
+  void *operator new(size_t, void *mem) noexcept {
+    assert(mem);
+    return mem;
+  }
+
+  Stmt(StmtKind kind) : kind(kind) {}
+
+public:
+  // Publicly allow allocation of expressions using the ASTContext.
+  void *operator new(size_t size, ASTContext &ctxt,
+                     unsigned align = alignof(Stmt));
+
+  /// \returns the SourceLoc of the first token of the statement
+  SourceLoc getBegLoc() const;
+  /// \returns the SourceLoc of the last token of the statement
+  SourceLoc getEndLoc() const;
+  /// \returns the full range of this expression
+  SourceRange getSourceRange() const;
+
+  /// \return the kind of statement this is
+  StmtKind getKind() const { return kind; }
+};
+} // namespace sora
