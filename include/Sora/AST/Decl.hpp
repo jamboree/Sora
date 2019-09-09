2b43691eff930564345d1c3e1e47ab7b2ebba599
Pierre van Houtryve <pierre.vanhoutryve@outlook.com>
2019-09-09 15:35:20 +0200

    Scaffolding Decls

--- include/Sora/AST/Decl.hpp
+++ include/Sora/AST/Decl.hpp
@@ -5,8 +5,54 @@
 // Copyright (c) 2019 Pierre van Houtryve
 //===----------------------------------------------------------------------===//
 
-#pragma once 
+#pragma once
+
+#include "Sora/AST/ASTAlignement.hpp"
+#include "Sora/Common/SourceLoc.hpp"
+#include <cassert>
+#include <stdint.h>
 
 namespace sora {
+class ASTContext;
 
-}
+/// Kinds of Declarations
+enum class DeclKind : uint8_t {
+#define DECL(KIND, PARENT) KIND,
+#define DECL_RANGE(KIND, FIRST, LAST) First_##KIND = FIRST, Last_##KIND = LAST,
+#include "Sora/AST/DeclNodes.def"
+};
+
+/// Base class for every Declaration node.
+class alignas(DeclAlignement) Decl {
+  // Disable vanilla new/delete for declarations
+  void *operator new(size_t) noexcept = delete;
+  void operator delete(void *)noexcept = delete;
+
+  DeclKind kind;
+
+protected:
+  // Children should be able to use placement new, as it is needed for children
+  // with trailing objects.
+  void *operator new(size_t, void *mem) noexcept {
+    assert(mem);
+    return mem;
+  }
+
+  Decl(DeclKind kind) : kind(kind) {}
+
+public:
+  // Publicly allow allocation of declaration using the ASTContext.
+  void *operator new(size_t size, ASTContext &ctxt,
+                     unsigned align = alignof(Decl));
+
+  /// \returns the SourceLoc of the first token of the declaration
+  SourceLoc getBegLoc() const;
+  /// \returns the SourceLoc of the last token of the declaration
+  SourceLoc getEndLoc() const;
+  /// \returns the full range of this declaration
+  SourceRange getSourceRange() const;
+
+  /// \return the kind of declaration this is
+  DeclKind getKind() const { return kind; }
+};
+} // namespace sora
--- lib/AST/Decl.cpp
+++ lib/AST/Decl.cpp
@@ -6,5 +6,92 @@
 //===----------------------------------------------------------------------===//
 
 #include "Sora/AST/Decl.hpp"
+#include "Sora/AST/ASTContext.hpp"
 
 using namespace sora;
+
+/// Check that all declarations are trivially destructible. This is needed
+/// because, as they are allocated in the ASTContext's arenas, their destructors
+/// are never called.
+#define DECL(ID, PARENT)                                                       \
+  static_assert(std::is_trivially_destructible<ID##Decl>::value,               \
+                #ID "Decl is not trivially destructible.");
+#include "Sora/AST/DeclNodes.def"
+
+namespace {
+template <typename Rtr, typename Class>
+constexpr bool isOverridenFromDecl(Rtr (Class::*)() const) {
+  return true;
+}
+
+template <typename Rtr>
+constexpr bool isOverridenFromDecl(Rtr (Decl::*)() const) {
+  return false;
+}
+
+/// Declarations can override (getSourceRange) or (getBegLoc & getEndLoc) or
+/// both. We adapt automatically based on what's available.
+template <typename Ty> struct DeclFetchLoc {
+  static constexpr bool hasGetRange = isOverridenFromDecl(&Ty::getSourceRange);
+  static constexpr bool hasGetBeg = isOverridenFromDecl(&Ty::getBegLoc);
+  static constexpr bool hasGetEnd = isOverridenFromDecl(&Ty::getEndLoc);
+
+  static_assert(hasGetRange || (hasGetBeg && hasGetEnd),
+                "Declarations must override (getSourceRange) or "
+                "(getBegLoc/getEndLoc) or both.");
+
+  static SourceRange getSourceRange(const Ty *decl) {
+    if (hasGetRange)
+      return decl->getSourceRange();
+    return {decl->getBegLoc(), stmt->getEndLoc()};
+  }
+
+  static SourceLoc getBegLoc(const Ty *decl) {
+    if (hasGetBeg)
+      return decl->getBegLoc();
+    return decl->getSourceRange().begin;
+  }
+
+  static SourceLoc getEndLoc(const Ty *decl) {
+    if (hasGetEnd)
+      return decl->getEndLoc();
+    return decl->getSourceRange().end;
+  }
+};
+} // namespace
+
+void *Decl::operator new(size_t size, ASTContext &ctxt, unsigned align) {
+  return ctxt.allocate(size, align, ASTAllocatorKind::Permanent);
+}
+
+SourceLoc Decl::getBegLoc() const {
+  switch (getKind()) {
+  default:
+    llvm_unreachable("unknown StmtKind");
+#define DECL(ID, PARENT)                                                       \
+  case DeclKind::ID:                                                           \
+    return DeclFetchLoc<ID##Decl>::getBegLoc(cast<ID##Decl>(this));
+#include "Sora/AST/DeclNodes.def"
+  }
+}
+
+SourceLoc Decl::getEndLoc() const {
+  switch (getKind()) {
+  default:
+    llvm_unreachable("unknown StmtKind");
+#define DECL(ID, PARENT)                                                       \
+  case DeclKind::ID:                                                           \
+    return DeclFetchLoc<ID##Decl>::getEndLoc(cast<ID##Decl>(this));
+#include "Sora/AST/DeclNodes.def"
+  }
+}
+SourceRange Decl::getSourceRange() const {
+  switch (getKind()) {
+  default:
+    llvm_unreachable("unknown StmtKind");
+#define DECL(ID, PARENT)                                                       \
+  case DeclKind::ID:                                                           \
+    return DeclFetchLoc<ID##Decl>::getSourceRange(cast<ID##Decl>(this));
+#include "Sora/AST/DeclNodes.def"
+  }
+}
