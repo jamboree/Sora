61e4e04f4bfa29196bd51d3b09db408215910e47
Pierre van Houtryve <pierre.vanhoutryve@outlook.com>
2019-09-10 15:43:03 +0200

    Allow ASTNode to work with Decl and Stmt

--- lib/AST/ASTNode.cpp
+++ lib/AST/ASTNode.cpp
@@ -16,35 +16,29 @@ using namespace sora;
 SourceRange ASTNode::getSourceRange() const {
   if (is<Expr *>())
     return get<Expr *>()->getSourceRange();
-  /*
   if (is<Decl *>())
     return get<Decl *>()->getSourceRange();
   if (is<Stmt *>())
     return get<Stmt *>()->getSourceRange();
-  */
   llvm_unreachable("unknown node");
 }
 
 SourceLoc ASTNode::getBegLoc() const {
   if (is<Expr *>())
     return get<Expr *>()->getBegLoc();
-  /*
   if (is<Decl *>())
     return get<Decl *>()->getBegLoc();
   if (is<Stmt *>())
     return get<Stmt *>()->getBegLoc();
-  */
   llvm_unreachable("unknown node");
 }
 
 SourceLoc ASTNode::getEndLoc() const {
   if (is<Expr *>())
     return get<Expr *>()->getEndLoc();
-  /*
   if (is<Decl *>())
     return get<Decl *>()->getEndLoc();
   if (is<Stmt *>())
     return get<Stmt *>()->getEndLoc();
-  */
   llvm_unreachable("unknown node");
 }
