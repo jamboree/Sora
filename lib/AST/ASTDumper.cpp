beb156db24a7fbc09611f6c95a6626c5903539a4
Pierre-vh <pierre.vanhoutryve@outlook.com>
2019-09-18 12:06:03 +0200

    Adding getKindStr to ASTDumper

--- lib/AST/ASTDumper.cpp
+++ lib/AST/ASTDumper.cpp
@@ -16,6 +16,7 @@
 using namespace sora;
 
 namespace {
+
 class Dumper : public SimpleASTVisitor<Dumper> {
   raw_ostream &out;
   unsigned indent;
@@ -23,7 +24,49 @@ class Dumper : public SimpleASTVisitor<Dumper> {
 public:
   Dumper(raw_ostream &out, unsigned indent) : out(out), indent(indent) {}
 
-  
+  const char *getKindStr(DeclKind kind) {
+    switch (kind) {
+    default:
+      llvm_unreachable("unknown DeclKind");
+#define DECL(ID, PARENT)                                                       \
+  case DeclKind::ID:                                                           \
+    return #ID "Decl";
+#include "Sora/AST/DeclNodes.def"
+    }
+  }
+
+  const char *getKindStr(ExprKind kind) {
+    switch (kind) {
+    default:
+      llvm_unreachable("unknown ExprKind");
+#define EXPR(ID, PARENT)                                                       \
+  case ExprKind::ID:                                                           \
+    return #ID "Expr";
+#include "Sora/AST/ExprNodes.def"
+    }
+  }
+
+  const char *getKindStr(StmtKind kind) {
+    switch (kind) {
+    default:
+      llvm_unreachable("unknown StmtKind");
+#define STMT(ID, PARENT)                                                       \
+  case StmtKind::ID:                                                           \
+    return #ID "Stmt";
+#include "Sora/AST/StmtNodes.def"
+    }
+  }
+
+  const char *getKindStr(PatternKind kind) {
+    switch (kind) {
+    default:
+      llvm_unreachable("unknown PatternKind");
+#define PATTERN(ID, PARENT)                                                    \
+  case PatternKind::ID:                                                        \
+    return #ID "Pattern";
+#include "Sora/AST/PatternNodes.def"
+    }
+  }
 };
 } // namespace
 
