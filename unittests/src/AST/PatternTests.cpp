7580c56c4684f46b1110e27f966ddb2bd94a3a3a
Pierre van Houtryve <pierre.vanhoutryve@outlook.com>
2019-09-23 16:11:06 +0200

    Refactor PatternTests

--- unittests/src/AST/PatternTests.cpp
+++ unittests/src/AST/PatternTests.cpp
@@ -16,92 +16,69 @@
 using namespace sora;
 
 namespace {
+const char *str = "Hello, World!";
+
 class PatternTest : public ::testing::Test {
 protected:
+  PatternTest() {
+    // Setup SourceLocs
+    beg = SourceLoc::fromPointer(str);
+    end = SourceLoc::fromPointer(str + 10);
+    // Setup nodes
+    varPattern = new (*ctxt) VarPattern(new (*ctxt) VarDecl(nullptr, beg, {}));
+    discardPattern = new (*ctxt) DiscardPattern(beg);
+    tuplePattern = TuplePattern::createEmpty(*ctxt, beg, end);
+    mutPattern = new (*ctxt) MutPattern(beg, new (*ctxt) DiscardPattern(end));
+    typedPattern =
+        new (*ctxt) TypedPattern(new (*ctxt) DiscardPattern(beg), {},
+                                 new (*ctxt) IdentifierTypeRepr(end, {}));
+  }
+
   SourceManager srcMgr;
   DiagnosticEngine diagEng{srcMgr, llvm::outs()};
   std::unique_ptr<ASTContext> ctxt{ASTContext::create(srcMgr, diagEng)};
+
+  SourceLoc beg, end;
+
+  Pattern *varPattern;
+  Pattern *discardPattern;
+  Pattern *mutPattern;
+  Pattern *tuplePattern;
+  Pattern *typedPattern;
 };
 } // namespace
 
 TEST_F(PatternTest, rtti) {
-  // VarPattern
-  {
-    Pattern *pattern = new (*ctxt) VarPattern(nullptr);
-    EXPECT_TRUE(isa<VarPattern>(pattern));
-  }
-
-  // DiscardPattern
-  {
-    Pattern *pattern = new (*ctxt) DiscardPattern({});
-    EXPECT_TRUE(isa<DiscardPattern>(pattern));
-  }
-
-  // TuplePattern
-  {
-    Pattern *pattern = TuplePattern::createEmpty(*ctxt, {}, {});
-    EXPECT_TRUE(isa<TuplePattern>(pattern));
-  }
-
-  // TypedPattern
-  {
-    Pattern *pattern = new (*ctxt) TypedPattern(nullptr, {}, nullptr);
-    EXPECT_TRUE(isa<TypedPattern>(pattern));
-  }
+  EXPECT_TRUE(isa<VarPattern>(varPattern));
+  EXPECT_TRUE(isa<DiscardPattern>(discardPattern));
+  EXPECT_TRUE(isa<MutPattern>(mutPattern));
+  EXPECT_TRUE(isa<TuplePattern>(tuplePattern));
+  EXPECT_TRUE(isa<TypedPattern>(typedPattern));
 }
 
 TEST_F(PatternTest, getSourceRange) {
-  const char *str = "Hello, World!";
-  SourceLoc beg = SourceLoc::fromPointer(str);
-  SourceLoc end = SourceLoc::fromPointer(str + 10);
-  SourceRange range(beg, end);
+  EXPECT_EQ(beg, varPattern->getBegLoc());
+  EXPECT_EQ(beg, varPattern->getLoc());
+  EXPECT_EQ(beg, varPattern->getEndLoc());
+  EXPECT_EQ(SourceRange(beg, beg), varPattern->getSourceRange());
 
-  // VarPattern
-  {
-    Pattern *pattern =
-        new (*ctxt) VarPattern(new (*ctxt) VarDecl(nullptr, beg, {}));
-    EXPECT_EQ(beg, pattern->getBegLoc());
-    EXPECT_EQ(beg, pattern->getLoc());
-    EXPECT_EQ(beg, pattern->getEndLoc());
-    EXPECT_EQ(SourceRange(beg, beg), pattern->getSourceRange());
-  }
+  EXPECT_EQ(beg, discardPattern->getBegLoc());
+  EXPECT_EQ(beg, discardPattern->getLoc());
+  EXPECT_EQ(beg, discardPattern->getEndLoc());
+  EXPECT_EQ(SourceRange(beg, beg), discardPattern->getSourceRange());
 
-  // DiscardPattern
-  {
-    Pattern *pattern = new (*ctxt) DiscardPattern(beg);
-    EXPECT_EQ(beg, pattern->getBegLoc());
-    EXPECT_EQ(beg, pattern->getLoc());
-    EXPECT_EQ(beg, pattern->getEndLoc());
-    EXPECT_EQ(SourceRange(beg, beg), pattern->getSourceRange());
-  }
+  EXPECT_EQ(beg, mutPattern->getBegLoc());
+  EXPECT_EQ(end, mutPattern->getLoc());
+  EXPECT_EQ(end, mutPattern->getEndLoc());
+  EXPECT_EQ(SourceRange(beg, end), mutPattern->getSourceRange());
 
-  // MutPattern
-  {
-    Pattern *pattern =
-        new (*ctxt) MutPattern(beg, new (*ctxt) DiscardPattern(end));
-    EXPECT_EQ(beg, pattern->getBegLoc());
-    EXPECT_EQ(end, pattern->getLoc());
-    EXPECT_EQ(end, pattern->getEndLoc());
-    EXPECT_EQ(range, pattern->getSourceRange());
-  }
+  EXPECT_EQ(beg, tuplePattern->getBegLoc());
+  EXPECT_EQ(beg, tuplePattern->getLoc());
+  EXPECT_EQ(end, tuplePattern->getEndLoc());
+  EXPECT_EQ(SourceRange(beg, end), tuplePattern->getSourceRange());
 
-  // TuplePattern
-  {
-    Pattern *pattern = TuplePattern::createEmpty(*ctxt, beg, end);
-    EXPECT_EQ(beg, pattern->getBegLoc());
-    EXPECT_EQ(beg, pattern->getLoc());
-    EXPECT_EQ(end, pattern->getEndLoc());
-    EXPECT_EQ(range, pattern->getSourceRange());
-  }
-
-  // TuplePattern
-  {
-    Pattern *pattern =
-        new (*ctxt) TypedPattern(new (*ctxt) DiscardPattern(beg), {},
-                                 new (*ctxt) IdentifierTypeRepr(end, {}));
-    EXPECT_EQ(beg, pattern->getBegLoc());
-    EXPECT_EQ(beg, pattern->getLoc());
-    EXPECT_EQ(end, pattern->getEndLoc());
-    EXPECT_EQ(range, pattern->getSourceRange());
-  }
+  EXPECT_EQ(beg, typedPattern->getBegLoc());
+  EXPECT_EQ(beg, typedPattern->getLoc());
+  EXPECT_EQ(end, typedPattern->getEndLoc());
+  EXPECT_EQ(SourceRange(beg, end), typedPattern->getSourceRange());
 }
