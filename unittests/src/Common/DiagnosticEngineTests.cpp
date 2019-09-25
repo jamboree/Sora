cd5262d90f0db8f35767044dda08d2b4965b0f96
Pierre-vh <pierre.vanhoutryve@outlook.com>
2019-09-25 10:19:11 +0200

    Removing "hello world" diagnostic

--- unittests/src/Common/DiagnosticEngineTests.cpp
+++ unittests/src/Common/DiagnosticEngineTests.cpp
@@ -5,7 +5,7 @@
 // Copyright (c) 2019 Pierre van Houtryve
 //===----------------------------------------------------------------------===//
 
-#include "Sora/Common/DiagnosticsCommon.hpp"
+#include "Sora/Common/DiagnosticsDriver.hpp"
 #include "Sora/Common/LLVM.hpp"
 #include "Sora/Common/SourceLoc.hpp"
 #include "Sora/Common/SourceManager.hpp"
@@ -36,7 +36,7 @@ TEST(DiagnosticEngineTest, diagnose) {
   llvm::raw_string_ostream stream(output);
 
   DiagnosticEngine diagEngine(srcMgr, stream);
-  diagEngine.diagnose(loc, diag::hello_world, "Pierre", 4)
+  diagEngine.diagnose(loc, diag::unknown_arg, "Pierre")
       .highlightChars(additionalRange)
       .fixitInsert(ins, "Incredibely")
       .fixitReplace(wordRange, "Hyperactive");
@@ -44,8 +44,7 @@ TEST(DiagnosticEngineTest, diagnose) {
   stream.str();
 
   ASSERT_NE(output.size(), 0) << "No Output.";
-  EXPECT_EQ(output, "some_file.sora:1:5: note: Hello, Pierre! I've greeted you "
-                    "4 times already!\n"
+  EXPECT_EQ(output, "some_file.sora:1:5: error: unknown argument 'Pierre'\n"
                     "The Lazy Brown Fox Jumps Over The Lazy Dog\n"
                     "    ^~~~\n"
                     "   Incredibely Hyperactive\n");
