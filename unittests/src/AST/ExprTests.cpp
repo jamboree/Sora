aaaaaa70c508ad7d9397fd0b593c945eeaa2453d
Pierre-vh <pierre.vanhoutryve@outlook.com>
2019-10-25 09:31:05 +0200

    Adding UnresolvedExpr test

--- unittests/src/AST/ExprTests.cpp
+++ unittests/src/AST/ExprTests.cpp
@@ -104,8 +104,24 @@ TEST_F(ExprTest, rtti) {
   EXPECT_TRUE(isa<UnaryExpr>(unaryExpr));
 }
 
+TEST_F(ExprTest, UnresolvedExprs) {
+  // Create another ASTContext in which we'll allocate a few unresolved expr so
+  // we can check that they're properly allocated using
+  // ArenaKind::UnresolvedExpr.
+  auto ctxt = ASTContext::create(srcMgr, diagEng);
+  ASSERT_EQ(ctxt->getMemoryUsed(ArenaKind::UnresolvedExpr), 0);
+  ASSERT_EQ(ctxt->getMemoryUsed(ArenaKind::TypeChecker), 0);
+  size_t basePerma = ctxt->getMemoryUsed(ArenaKind::Permanent);
+  new (*ctxt) UnresolvedDeclRefExpr({}, {});
+  new (*ctxt) UnresolvedMemberRefExpr(nullptr, {}, false, {}, {});
+  EXPECT_NE(ctxt->getMemoryUsed(ArenaKind::UnresolvedExpr), 0);
+  EXPECT_EQ(ctxt->getMemoryUsed(ArenaKind::Permanent), basePerma);
+  EXPECT_EQ(ctxt->getMemoryUsed(ArenaKind::TypeChecker), 0);
+}
+
 TEST_F(ExprTest, getSourceRange) {
   // UnresolvedDeclRefExpr
+  
   EXPECT_EQ(beg, unresDeclRefExpr->getBegLoc());
   EXPECT_EQ(beg, unresDeclRefExpr->getLoc());
   EXPECT_EQ(beg, unresDeclRefExpr->getEndLoc());
