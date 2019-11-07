98cc815c044c482c21bbc5a81dd922ea54edeaa6
Pierre-vh <pierre.vanhoutryve@outlook.com>
2019-11-07 12:07:27 +0100

    Adding "performSema" w/ a test implementation

--- include/Sora/Sema/Sema.hpp
+++ include/Sora/Sema/Sema.hpp
@@ -11,6 +11,7 @@
 
 namespace sora {
 class ASTContext;
+class SourceFile;
 
 /// Main interface of Sora's semantic analyzer.
 class Sema final {
@@ -20,13 +21,17 @@ class Sema final {
 public:
   Sema(ASTContext &ctxt);
 
+  /// Main entry point of semantic analysis: performs semantic analysis on \p
+  /// file.
+  void performSema(SourceFile &file);
+
   /// Emits a diagnostic at \p loc
   template <typename... Args>
   InFlightDiagnostic
   diagnose(SourceLoc loc, TypedDiag<Args...> diag,
            typename detail::PassArgument<Args>::type... args) {
     assert(loc && "Sema can't emit diagnostics without valid SourceLocs");
-    return diagEng.diagnose<Args...>(loc, diag, args...);
+    return diagEngine.diagnose<Args...>(loc, diag, args...);
   }
 
   ASTContext &ctxt;
--- lib/Sema/Sema.cpp
+++ lib/Sema/Sema.cpp
@@ -11,3 +11,47 @@
 using namespace sora;
 
 Sema::Sema(ASTContext &ctxt) : ctxt(ctxt), diagEngine(ctxt.diagEngine) {}
+
+//===----------------------------------------------------------------------===//
+// This is currently just for testing purposes and will be refactored later.
+// We're just going to perform name binding on every UnresolvedDeclRef in
+// the source file.
+//===----------------------------------------------------------------------===//
+
+#include "Sora/AST/ASTWalker.hpp"
+#include "Sora/AST/Decl.hpp"
+#include "Sora/AST/Expr.hpp"
+#include "Sora/AST/NameLookup.hpp"
+#include "Sora/AST/SourceFile.hpp"
+#include "Sora/Diagnostics/DiagnosticsSema.hpp"
+#include "llvm/Support/raw_ostream.h"
+
+void Sema::performSema(SourceFile &file) {
+  struct Impl : ASTWalker {
+    Sema &sema;
+    SourceFile &file;
+
+    Impl(Sema &sema, SourceFile &file) : sema(sema), file(file) {}
+
+    std::pair<bool, Expr *> walkToExprPost(Expr *expr) override {
+      auto *udre = dyn_cast<UnresolvedDeclRefExpr>(expr);
+      if (!udre)
+        return {true, expr};
+      UnqualifiedLookup ul(file);
+      ul.performLookup(udre->getLoc(), udre->getIdentifier());
+      llvm::outs() << "-----------------------------------\n";
+      llvm::outs() << "Expression:\n";
+      udre->dump(llvm::outs(), sema.ctxt.srcMgr);
+      llvm::outs() << "Results found: " << ul.results.size() << "\n";
+      unsigned k = 0;
+      for (ValueDecl *result : ul.results) {
+        llvm::outs() << "Result #" << k++ << ":";
+        result->dump(llvm::outs());
+        llvm::outs() << "\n";
+      }
+      return {true, expr};
+    }
+  };
+  llvm::outs() << "Attempting to find candidates\n";
+  file.walk(Impl(*this, file));
+}
