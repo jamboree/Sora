6990b6a3ecf9912f897db06393e0e5db93d580d7
Pierre-vh <pierre.vanhoutryve@outlook.com>
2019-11-22 11:33:47 +0100

    ConstraintSystem WIP

--- lib/Sema/ConstraintSystem.cpp
+++ lib/Sema/ConstraintSystem.cpp
@@ -6,10 +6,164 @@
 //===----------------------------------------------------------------------===//
 
 #include "ConstraintSystem.hpp"
+#include "Sora/AST/TypeVisitor.hpp"
 #include "Sora/AST/Types.hpp"
+#include "llvm/Support/raw_ostream.h"
 
 using namespace sora;
 
+//===--- TypeSimplifier ---------------------------------------------------===//
+
+namespace {
+/// The TypeVariableType simplifier, which replaces type variables with their
+/// substitutions.
+///
+/// Note that visit methods return null when no substitutions are needed, so
+/// visit() returns null for type with no type variables.
+class TypeSimplifier : public TypeVisitor<TypeSimplifier, Type> {
+public:
+  using Parent = TypeVisitor<TypeSimplifier, Type>;
+  friend Parent;
+
+  TypeSimplifier(ConstraintSystem &cs) : cs(cs) {}
+
+  ConstraintSystem &cs;
+
+  using Parent::visit;
+
+private:
+  Type visitBuiltinType(BuiltinType *type) { return nullptr; }
+
+  Type visitReferenceType(ReferenceType *type) {
+    if (Type simplified = visit(type->getPointeeType()))
+      return ReferenceType::get(simplified, type->isMut());
+    return nullptr;
+  }
+
+  Type visitMaybeType(MaybeType *type) {
+    if (Type simplified = visit(type->getValueType()))
+      return MaybeType::get(simplified);
+    return nullptr;
+  }
+
+  Type visitTupleType(TupleType *type) {
+    bool needsRebuilding = false;
+
+    SmallVector<Type, 4> elems;
+    elems.reserve(type->getNumElements());
+    for (Type elem : type->getElements()) {
+      if (Type simplified = visit(elem)) {
+        needsRebuilding = true;
+        elems.push_back(simplified);
+      }
+      else
+        elems.push_back(elem);
+    }
+
+    if (!needsRebuilding)
+      return nullptr;
+    return TupleType::get(cs.ctxt, elems);
+  }
+
+  Type visitFunctionType(FunctionType *type) {
+    bool needsRebuilding = false;
+
+    Type rtr = type->getReturnType();
+    if (Type simplified = visit(rtr)) {
+      needsRebuilding = true;
+      rtr = simplified;
+    }
+
+    SmallVector<Type, 4> args;
+    args.reserve(type->getNumArgs());
+    for (Type arg : type->getArgs()) {
+      if (Type simplified = visit(arg)) {
+        needsRebuilding = true;
+        args.push_back(simplified);
+      }
+      else
+        args.push_back(arg);
+    }
+
+    if (!needsRebuilding)
+      return nullptr;
+    return FunctionType::get(args, rtr);
+  }
+
+  Type visitLValueType(LValueType *type) {
+    if (Type simplified = visit(type->getObjectType()))
+      return LValueType::get(simplified);
+    return nullptr;
+  }
+
+  Type visitErrorType(ErrorType *type) { return nullptr; }
+
+  Type visitTypeVariableType(TypeVariableType *type) {
+    Type subst = cs.getInfo(type).getSubstitution();
+    // Return ErrorType when there are no substitutions
+    if (!subst)
+      return cs.ctxt.errorType;
+    // Recursively simplify TypeVariableTypes
+    if (Type simplified = visit(subst))
+      return simplified;
+    return subst;
+  }
+};
+} // namespace
+
+//===--- ConstraintSystem -------------------------------------------------===//
+
 TypeVariableType *ConstraintSystem::createTypeVariable() {
   return TypeVariableType::create(ctxt, nextTypeVariableID++);
+}
+
+Type ConstraintSystem::simplifyType(Type type) {
+  if (!type->hasTypeVariable())
+    return type;
+  Type simplified = TypeSimplifier(*this).visit(type);
+  assert(simplified && !type->hasTypeVariable() && "Not simplified!");
+  return simplified;
+}
+
+void ConstraintSystem::print(raw_ostream &out, const TypeVariableType *type,
+                             const TypePrintOptions &printOptions) const {
+  // Print the TypeVariable itself
+  type->print(out, printOptions);
+  // Print the kind
+  TypeVariableInfo &info = getInfo(type);
+  switch (info.getTypeVariableKind()) {
+  case TypeVariableKind::General:
+    out << " [general type variable]";
+    break;
+  case TypeVariableKind::Integer:
+    out << " [integer type variable]";
+    break;
+  case TypeVariableKind::Float:
+    out << " [float type variable]";
+    break;
+  }
+  // Print the substitution
+  if (Type type = info.getSubstitution()) {
+    out << " bound to '";
+    type.print(out, printOptions);
+    out << "'";
+  }
+  else
+    out << " unbound";
+}
+
+std::string
+ConstraintSystem::getString(const TypeVariableType *type,
+                            const TypePrintOptions &printOptions) const {
+  std::string str;
+  llvm::raw_string_ostream out(str);
+  print(out, type, printOptions);
+  return out.str();
+}
+
+TypeVariableInfo &
+ConstraintSystem::getInfo(const TypeVariableType *type) const {
+  TypeVariableInfo *info = typeVariableInfos[type->getID()];
+  assert(info && "No info for TypeVariable?");
+  return *info;
 }
--- lib/Sema/ConstraintSystem.hpp
+++ lib/Sema/ConstraintSystem.hpp
@@ -8,12 +8,66 @@
 #pragma once
 
 #include "Sora/AST/ASTContext.hpp"
+#include "Sora/AST/Type.hpp"
 #include "Sora/Common/LLVM.hpp"
 #include "TypeChecker.hpp"
 #include "llvm/ADT/SmallVector.h"
+#include <string>
 
 namespace sora {
+class ConstraintSystem;
 class TypeVariableType;
+
+/// Kinds of Type Variables
+enum class TypeVariableKind : uint8_t {
+  /// Can unify w/ any type
+  General,
+  /// Can unify w/ integer types
+  Integer,
+  /// Can unify w/ float types
+  Float
+};
+
+/// TypeVariable extra information
+class TypeVariableInfo final {
+  friend class ConstraintSystem;
+
+  TypeVariableInfo(TypeVariableKind tvKind) : tvKind(tvKind) {}
+
+  /// The kind of TypeVariable this is
+  TypeVariableKind tvKind;
+  /// This TypeVariable's substitution.
+  /// Note that this can be another TypeVariable in case of an equivalence.
+  Type substitution;
+
+public:
+  TypeVariableKind getTypeVariableKind() const { return tvKind; }
+
+  bool isGeneralTypeVariable() const {
+    return getTypeVariableKind() == TypeVariableKind::General;
+  }
+
+  bool isIntegerTypeVariable() const {
+    return getTypeVariableKind() == TypeVariableKind::Integer;
+  }
+
+  bool isFloatTypeVariable() const {
+    return getTypeVariableKind() == TypeVariableKind::Float;
+  }
+
+  /// Sets this TypeVariable's substitution. This can only be done once (\c
+  /// hasSubstitution() must return false)
+  void setSubstitution(Type type) {
+    assert(!hasSubstitution() && "Already has a substitution");
+    substitution = type;
+  }
+  /// \returns true if this TypeVariable has a substitution
+  bool hasSubstitution() const { return (bool)substitution; }
+  /// \returns this TypeVariable's substitution (it can be a TypeVariable in
+  /// case of an equivalence class, see \c getBoundSubstitution() to get a
+  /// non TypeVariable substitution)
+  Type getSubstitution() const { return substitution; }
+};
 
 /// The ConstraintSystem serves a context for constraint generation, solving
 /// and diagnosing.
@@ -23,9 +77,16 @@ class TypeVariableType;
 ///
 /// It also possesses a RAIIConstraintSystemArena object, so while this object
 /// is alive, the ConstraintSystem arena is available.
+///
+/// Note that only one of those can be active at once, and that this is a
+/// relatively large object that can't be copied.
 class ConstraintSystem final {
+public:
   ConstraintSystem(TypeChecker &tc)
       : ctxt(tc.ctxt), raiiCSArena(ctxt.createConstraintSystemArena()) {}
+
+  ConstraintSystem(const ConstraintSystem &) = delete;
+  ConstraintSystem &operator=(const ConstraintSystem &) = delete;
 
   ASTContext &ctxt;
 
@@ -37,8 +98,27 @@ private:
   /// system.
   unsigned nextTypeVariableID = 0;
 
+  /// The array of type variable informations.
+  SmallVector<TypeVariableInfo *, 16> typeVariableInfos;
+
 public:
   /// Creates a new type variable.
   TypeVariableType *createTypeVariable();
+
+  /// Simplifies \p type, replacing type variables with their substitution (or
+  /// ErrorType if there's no substitution)
+  Type simplifyType(Type type);
+
+  /// Prints \p type and its TypeVariableInfo to \p out
+  void print(raw_ostream &out, const TypeVariableType *type,
+             const TypePrintOptions &printOptions = TypePrintOptions()) const;
+
+  /// Same as \c print, but returns a string instead of printing to a stream.
+  std::string
+  getString(const TypeVariableType *type,
+            const TypePrintOptions &printOptions = TypePrintOptions()) const;
+
+  /// \returns the TypeVariableKind of \p type
+  TypeVariableInfo &getInfo(const TypeVariableType *type) const;
 };
 } // namespace sora
