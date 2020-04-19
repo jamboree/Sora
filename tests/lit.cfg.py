da3afc94e228414baadaf7b8bfed112aabf1fbe4
Pierre van Houtryve <pierre.vanhoutryve@outlook.com>
2020-04-19 16:50:53 +0200

    Simplify most test commands
    - Remove the substitutions, use PATH instead for the executables. Much cleaner!

---
+++ tests/lit.cfg.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import lit
+import lit.formats
+import subprocess
+
+config.name = 'Sora Unit Tests'
+config.suffixes = ['.sora']
+config.test_format = lit.formats.ShTest(execute_external=False)
+config.test_source_root = os.path.dirname(__file__)
+
+# Check if sorac is available
+try:
+    subprocess.check_output(["sorac", "-h"])
+except OSError as e:
+  lit_config.error("Can't find the Sora binary in lit parameters or PATH")
+  exit(1)
+
+# Check if FileCheck is available
+try:
+    subprocess.check_output(["FileCheck", "--help"])
+except OSError as e:
+  lit_config.error("Can't find LLVM FileCheck")
+  exit(1)
