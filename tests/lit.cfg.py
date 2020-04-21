4ab59b09836c098b95ca2855135c3aba9d6a713a
Pierre van Houtryve <pierre.vanhoutryve@outlook.com>
2020-04-21 17:18:45 +0200

    Add FileCheck test-generation script

--- tests/lit.cfg.py
+++ tests/lit.cfg.py
@@ -11,14 +11,18 @@ config.test_source_root = os.path.dirname(__file__)
 
 # Check if sorac is available
 try:
-    subprocess.check_output(["sorac", "-h"])
+  subprocess.check_output(["sorac", "-h"])
 except OSError as e:
   lit_config.error("Can't find the Sora binary in lit parameters or PATH")
   exit(1)
 
 # Check if FileCheck is available
 try:
-    subprocess.check_output(["FileCheck", "--help"])
+  subprocess.check_output(["FileCheck", "--help"])
 except OSError as e:
   lit_config.error("Can't find LLVM FileCheck")
   exit(1)
+
+for (param, value) in lit_config.params.items():
+  if param == "FileCheck":
+    config.substitutions.append((param, value))
